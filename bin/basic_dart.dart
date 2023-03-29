3dc83275409f9e4b8ea301d32b582fb679fc601e
Mishalpopy <c.mishal46@gmail.com>
2023-03-29 17:31:47 +0400

    first commit

---
+++ bin/basic_dart.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() {
+  // print("Hello World");
+  // var a = stdin.readLineSync();
+  // print('Hello $a');
+
+  // print("Enter 2 Numbers");
+  // var input1 = stdin.readLineSync();
+  // var input2 = stdin.readLineSync();
+
+  // if (input1 == null) {
+  //   input1 = '0';
+  // }
+
+  // var num1 = int.parse(input1!);
+  // var num2 = int.parse(input2!);
+
+  // print('SUM = ${num1 + num2}');
+
+  int a = 10;
+  double b = 20;
+  num num1 = 2.0;
+  num num2 = 10;
+}
